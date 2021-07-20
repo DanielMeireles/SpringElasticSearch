961c9e4a7d90559aed77b1d89f0cc2512d959363
Daniel Meireles <daniel.meireles@outlook.com>
2021-07-20 10:22:55 -0300

    Project creation

---
+++ src/test/java/br/com/dmeireles/springelasticsearch/SpringElasticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dmeireles.springelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElasticSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
